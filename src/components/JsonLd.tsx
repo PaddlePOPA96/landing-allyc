aa6af9c341b7f6c33ca00c573acba077b99e0b7a
PaddlePOPA96 <dutahad@gmail.com>
2026-01-05 17:58:28 +0700

    test url

--- src/components/JsonLd.tsx
+++ src/components/JsonLd.tsx
@@ -1,22 +1,27 @@
 export default function JsonLd() {
     const jsonLd = {
         '@context': 'https://schema.org',
-        '@type': 'Person',
-        name: 'Jasmine Allyc',
-        alternateName: 'RRQ Allyc',
-        description: 'Valorant Streamer & Content Creator',
-        url: 'https://allyc.vercel.app',
-        image: 'https://allyc.vercel.app/hero-image.webp',
-        sameAs: [
-            'https://www.instagram.com/jasmine.allyc/',
-            'https://www.tiktok.com/@jasmine.mat',
-            'https://www.youtube.com/@Allyccc',
-            'https://saweria.co/Allyc'
-        ],
-        jobTitle: 'Streamer',
-        worksFor: {
-            '@type': 'Organization',
-            name: 'RRQ'
+        '@type': 'ProfilePage',
+        dateCreated: '2025-01-01T00:00:00+07:00',
+        dateModified: new Date().toISOString(),
+        mainEntity: {
+            '@type': 'Person',
+            name: 'Jasmine Allyc',
+            alternateName: 'RRQ Allyc',
+            description: 'Valorant Streamer & Content Creator',
+            url: 'https://allyc.vercel.app',
+            image: 'https://allyc.vercel.app/hero-image.webp',
+            sameAs: [
+                'https://www.instagram.com/jasmine.allyc/',
+                'https://www.tiktok.com/@jasmine.mat',
+                'https://www.youtube.com/@Allyccc',
+                'https://saweria.co/Allyc'
+            ],
+            jobTitle: 'Streamer',
+            worksFor: {
+                '@type': 'Organization',
+                name: 'RRQ'
+            }
         }
     }
 
